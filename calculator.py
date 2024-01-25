486606458c6af25caf041cddb1bed16e9b4b487d
Arda Kayıboğlu <ardakybgl@gmail.com>
2024-01-25 17:18:40 +0300

    fixed bugs

--- calculator.py
+++ calculator.py
@@ -1,18 +1,27 @@
 def main():
-    operation = input("What do you want to do?(+, -, *, or /):")
+    operation = input("What do you want to do?(+, -, *-, or /):")
     if (operation != "+" and operation != "-" and operation != "*" and operation != "/"):
         print("Your input is invalid. Please enter a valid input.")
     else:
         num1 = float(input("Enter value for num1: "))
         num2 = float(input("Enter value for num2: "))
         if (operation == "+"):
-            print(add(num1, num2))
+            print(num1 + num2)
         elif (operation == "-"):
-            print(subtract(num1, num2))
+            print(num1 - num2)
         elif (operation == "*"):
-            print(multi(num1,num2))
+            print(num1 * num2)
         elif (operation == "/"):
-            print(div(num1,num2))
+            if(num2!=0):
+                print(num1 / num2)
+            else:
+                print("you cant divide with 0")
+        
 
-if __name__ == "__main__":
-    main() 
+if __name__ == "__main__": 
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        pass
+    #For this the exit keystroke would be ctrl+c
